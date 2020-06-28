748b975d9f24e4359050c697a136d0cb6c0600ca
Jaffer <Jaffer@LAPTOP-78A3O0R9>
2020-06-29 02:37:05 +0530

    Signed-off-by: Jaffer <Jaffer@LAPTOP-78A3O0R9>

--- util/src/main/java/com/winnovature/unitia/util/misc/ACKIdGenerator.java
+++ util/src/main/java/com/winnovature/unitia/util/misc/ACKIdGenerator.java
@@ -5,7 +5,7 @@ import com.winnovature.unitia.util.instance.InstanceInfoMemory;
 
 public class ACKIdGenerator {
 
-	public static int ackIdIncrement = 10000;
+	public static int ackIdIncrement = 1000000;
 	
 
     public static synchronized String getAckId() {
@@ -13,7 +13,7 @@ public class ACKIdGenerator {
 		StringBuffer s_buffer = new StringBuffer();		
 		s_buffer.append(InstanceInfoMemory.INSTANCE_ID);		
 		s_buffer.append(System.currentTimeMillis());		
-		if (ackIdIncrement > 99990) ackIdIncrement = 10000;
+		if (ackIdIncrement > 9999990) ackIdIncrement = 1000000;
 		s_buffer.append(++ackIdIncrement);
 		
 		return s_buffer.toString();
