b3f84e74ca1ecacdb745ede0978e1bd951acc512
Jaffer <Jaffer@LAPTOP-78A3O0R9>
2020-06-29 03:02:13 +0530

    Signed-off-by: Jaffer <Jaffer@LAPTOP-78A3O0R9>

--- util/src/main/java/com/winnovature/unitia/util/misc/ACKIdGenerator.java
+++ util/src/main/java/com/winnovature/unitia/util/misc/ACKIdGenerator.java
@@ -1,20 +1,26 @@
 
 package com.winnovature.unitia.util.misc;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.winnovature.unitia.util.instance.InstanceInfoMemory;
 
 public class ACKIdGenerator {
 
-	public static int ackIdIncrement = 1000000;
+    private static AtomicInteger count = new AtomicInteger();
 	
 
     public static synchronized String getAckId() {
         
 		StringBuffer s_buffer = new StringBuffer();		
 		s_buffer.append(InstanceInfoMemory.INSTANCE_ID);		
-		s_buffer.append(System.currentTimeMillis());		
-		if (ackIdIncrement > 9999990) ackIdIncrement = 1000000;
-		s_buffer.append(++ackIdIncrement);
+		s_buffer.append(System.currentTimeMillis());
+		int seq=count.getAndIncrement();
+		if (seq > 9990){
+			count.set(1000);
+			seq=count.getAndIncrement();
+		}
+		s_buffer.append(seq);
 		
 		return s_buffer.toString();
         
