78847f533a758e886b7b223e06ae276bacc7efb6
Jaffer <Jaffer@LAPTOP-78A3O0R9>
2020-06-29 20:22:49 +0530

    Signed-off-by: Jaffer <Jaffer@LAPTOP-78A3O0R9>

--- unitiahttpd/src/main/java/unitiahttpd/App.java
+++ unitiahttpd/src/main/java/unitiahttpd/App.java
@@ -1,65 +1,85 @@
 package unitiahttpd;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-import javaxt.http.servlet.HttpServlet;
-import javaxt.http.servlet.HttpServletRequest;
-import javaxt.http.servlet.HttpServletResponse;
-import javaxt.http.servlet.ServletException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class App {
- 
-  //Entry point for the application
-    public static void main(String[] args) {
- 
-      //Start the server
-        try {
-        	
-        	new T().start();
-            int port = 8080;
-            int numThreads = 50;
-            javaxt.http.Server server = new javaxt.http.Server(port, numThreads, new TestServlet());
-            server.start();
-        }
-        catch (Exception e) {
-            System.out.println("Server could not start because of an " + e.getClass());
-            System.exit(1);
-        }
-    }
- 
-  //Custom Servlet
-    private static class TestServlet extends HttpServlet {
- 
-        public void processRequest(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, java.io.IOException {
- 
-            response.write("Hello, the time is now " + new java.util.Date());
-        }
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import com.winnovature.unitia.util.misc.Bean;
+import com.winnovature.unitia.util.misc.Log;
+
+public class App extends AbstractHandler
+{
+    @Override
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response) throws IOException,
+        ServletException
+    {
+        // Declare response encoding and types
+        response.setContentType("text/html; charset=utf-8");
+
+        // Declare response status code
+        response.setStatus(HttpServletResponse.SC_OK);
+
         
+        Map<String,String> msgmap=getMap(request);
 
+        Map<String, String> logmap = new HashMap<String,String>();
+		RequestProcessor processor = new RequestProcessor();
+		Bean.setDefaultValues(msgmap);
+		String responsestring=processor.processRequest(msgmap, logmap);		
+		logmap.putAll(msgmap);
+		new Log().log(logmap);
+        response.getWriter().println(responsestring);
 
-    	private Map<String, String> getMap(HttpServletRequest request) {
-    		
-            final StringTokenizer st = new StringTokenizer(request.getQueryString(), "&");
-            final HashMap reqParam = new HashMap();
-            while (st.hasMoreTokens())
+        // Inform jetty that this request has now been handled
+        baseRequest.setHandled(true);
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+    	new T().start();
+
+        Server server = new Server(8080);
+        server.setHandler(new App());
+
+        server.start();
+        server.join();
+    }
+    
+
+	private Map<String, String> getMap(HttpServletRequest request) {
+		
+        final StringTokenizer st = new StringTokenizer(request.getQueryString(), "&");
+        final HashMap reqParam = new HashMap();
+        while (st.hasMoreTokens())
+        {
+            final StringTokenizer st2 = new StringTokenizer(st.nextToken(), "=");
+            String key = "";
+            String value = "";
+            if (st2.hasMoreTokens())
             {
-                final StringTokenizer st2 = new StringTokenizer(st.nextToken(), "=");
-                String key = "";
-                String value = "";
+                key = st2.nextToken();
                 if (st2.hasMoreTokens())
                 {
-                    key = st2.nextToken();
-                    if (st2.hasMoreTokens())
-                    {
-                        value = st2.nextToken();
-                    }
+                    value = st2.nextToken();
                 }
-                reqParam.put(key, value);
             }
-            return reqParam;
+            reqParam.put(key, value);
         }
+        return reqParam;
     }
+		
+
 }
+
