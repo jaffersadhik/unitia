5e45356188b39a1c44e3d7afe6684dd29a30834a
Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>
2020-10-22 18:21:49 +0530

    Signed-off-by: Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>

--- util/src/main/java/com/winnovature/unitia/util/processor/DNHttpPost.java
+++ util/src/main/java/com/winnovature/unitia/util/processor/DNHttpPost.java
@@ -50,12 +50,10 @@ public class DNHttpPost
 			attempttype="0";
 		}
 		
-		System.out.println("attempttype : "+attempttype);
 
 		if(attempttype!=null&&attempttype.equals("9")){
 			
 			String url=MissedCallForward.getInstance().getUrl(msgmap.get(MapKeys.PARAM2).toString());
-			System.out.println("url : "+url);
 
 			Map<String,String> extraparam=getExtraParam(url);
 			msgmap.put("clinet_url", url);
@@ -66,9 +64,9 @@ public class DNHttpPost
 			
 		String url=PushAccount.instance().getPushAccount(msgmap.get(MapKeys.USERNAME).toString()).get(MapKeys.DLR_POST_URL);
     
+
 		Map<String,String> extraparam=getExtraParam(url);
 
-		System.out.println("url : "+url);
 
 		msgmap.put("clinet_url", url);
 		
@@ -79,6 +77,10 @@ public class DNHttpPost
     
     private Map<String, String> getExtraParam(String urls) {
 		
+    		if(!(urls.indexOf("?")>0)){
+    			
+    			return new HashMap<String,String>();
+    		}
     	   URL url=null;
 			try {
 				url = new URL(urls);
@@ -88,8 +90,14 @@ public class DNHttpPost
 			 Map<String,String> reqmap=null;
 			if(url!=null){
 				
+				String qs=url.getQuery();
+				if(qs!=null){
 				reqmap= getRequestParam(url.getQuery());
+				}else{
 
+					reqmap=new HashMap<String,String>();
+					
+				}
 			}else{
 				reqmap=new HashMap<String,String>();
 				
@@ -133,9 +141,7 @@ public class DNHttpPost
 					}
 					url+=getURLParams(attempttype,extraparam);
 
-					System.out.println("url : "+url);
 					response=deliverThroughURL(url);
-					System.out.println("response : "+response);
 
 				}
 				
