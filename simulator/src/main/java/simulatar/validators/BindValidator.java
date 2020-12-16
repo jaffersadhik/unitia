4b9fa6ef739ac5aaf5ce1a5e9f596da57cd2c991
Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>
2020-12-16 23:55:41 +0530

    Signed-off-by: Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>

--- simulator/src/main/java/simulatar/validators/BindValidator.java
+++ simulator/src/main/java/simulatar/validators/BindValidator.java
@@ -1,5 +1,7 @@
 package simulatar.validators;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import com.cloudhopper.smpp.SmppConstants;
@@ -12,13 +14,33 @@ public class BindValidator {
 	
 	final static String className="[Authenticator]";
 	
+	static List<String> users=new ArrayList<String>();
+	
+	static{
+		users.add("smppclient1");
+		users.add("smppclient2");
+		users.add("smppclient3");
+		users.add("smppclient4");
+		users.add("smppclient5");
+		users.add("smppclient6");
+		users.add("smppclient7");
+		users.add("smppclient8");
+		users.add("smppclient9");
+		users.add("smppclient10");
+		users.add("smppclient11");
+		users.add("smppclient12");
+		users.add("smppclient13");
+		users.add("smppclient14");
+		users.add("smppclient15");
+	}
+	
 	public void validate(BaseBind bindRequest, String hostip, Map<String, Object> logmap) throws SmppProcessingException {
 		
 				String systemId = bindRequest.getSystemId().toLowerCase();
 		    	String password = bindRequest.getPassword();
     			logmap.put("username", systemId);
 
-	    		if(systemId == null || systemId.trim().length() == 0 || !systemId.equals("smppclient1")) {
+	    		if(systemId == null || systemId.trim().length() == 0 || !users.contains(systemId)) {
 
 	    			logmap.put("status", "Invalid System ID : systemid is :"+systemId);
 	    			throw new SmppProcessingException(SmppConstants.STATUS_INVSYSID);
