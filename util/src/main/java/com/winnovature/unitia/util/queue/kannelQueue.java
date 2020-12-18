9acc9824b111e2898c68a6ac314468606a9417fb
Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>
2020-12-18 16:17:24 +0530

    Signed-off-by: Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>

---
+++ util/src/main/java/com/winnovature/unitia/util/queue/kannelQueue.java
@@ -0,0 +1,151 @@
+package com.winnovature.unitia.util.queue;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import java.util.StringTokenizer;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import com.winnovature.unitia.util.db.Close;
+import com.winnovature.unitia.util.db.CoreDBConnection;
+import com.winnovature.unitia.util.db.Kannel;
+import com.winnovature.unitia.util.db.TableExsists;
+import com.winnovature.unitia.util.misc.SMSCMaxQueue;
+
+public class kannelQueue {
+
+	static Map<String,Map<String,String>> smscqueue=new HashMap<String,Map<String,String>>();
+	
+	static{
+		
+
+			Connection connection=null;
+			
+			try{
+				connection=CoreDBConnection.getInstance().getConnection();
+				TableExsists table=new TableExsists();
+				
+				if(!table.isExsists(connection, "queue_count_smscid")){
+					
+					table.create(connection, "create table queue_count_smscid(smscid varchar(50),queuecount numeric(10,0),status varchar(20),updatetime numeric(13,0))", false);
+				}
+				
+				
+				
+			}catch(Exception e){
+				 e.printStackTrace();
+			}finally{
+			
+				Close.close(connection);
+			}
+			
+		
+	}
+	public static void reload() {
+		
+		smscqueue=getQueueFromDB();
+	}
+	
+	
+	public static Map<String,Map<String,String>> getQueueFromDB() {
+		
+		Connection connection=null;
+		PreparedStatement select=null;
+        ResultSet resultset=null;
+    	Map<String,Map<String,String>> result=new HashMap<String,Map<String,String>>();
+
+		try
+		
+		{
+			
+
+			
+			connection =CoreDBConnection.getInstance().getConnection();
+			select=connection.prepareStatement("select smscid,queuecount,status from queue_count_smscid");
+            resultset=select.executeQuery();
+			
+
+			while(resultset.next()){
+				
+				String smscid=resultset.getString("smscid");
+				String queuecount=resultset.getString("queuecount");
+				String status=resultset.getString("status");
+				Map<String,String> data=new HashMap<String,String>();
+				data.put("queued", queuecount);
+				data.put("status", status);
+
+				result.put(smscid, data);
+			}
+			
+			
+		
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			
+			Close.close(resultset);
+			Close.close(select);
+			Close.close(connection);
+		}
+		
+		return result;
+	}
+	 
+	 
+	 public static boolean isQueued(String smscid){
+		 
+		 Map<String,String> data=smscqueue.get(smscid);
+		 
+		 if(data==null){
+			 
+			 return false;
+		 }
+		 
+		 String status=data.get("status");
+		 
+		 if(status==null){
+			 
+			 return false;
+		 }
+		 
+		 
+		 if(status.equals("down")){
+			 
+			 return true;
+		 }
+		 
+		 
+		 String queued=data.get("queued");
+		 
+		 if(queued==null){
+			 
+			 return false;
+		 }
+		 
+		 
+		 String maxqueue=SMSCMaxQueue.getInstance().getQueue(smscid);
+		 
+		 long lMaxQueue=Long.parseLong(maxqueue);
+		 
+		 long lQueued=Long.parseLong(queued);
+		 
+		 return lQueued>lMaxQueue;
+	 }
+}
+
