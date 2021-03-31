5cd9c11d52d115742b7c02734ed23efd6958c28b
Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>
2021-04-01 04:18:46 +0530

    Signed-off-by: Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>

--- unitiaroute/src/main/java/unitiaroute/T1.java
+++ unitiaroute/src/main/java/unitiaroute/T1.java
@@ -8,95 +8,7 @@ public class T1 {
 		
 		System.out.println("======true=========");
 		T1 obj=new T1();
-		obj.m1();
-		obj.m2();
-		obj.m3();
-		obj.m4(); 
-		obj.m5();
-		obj.m6();
-		obj.m7();
-		obj.m8();
-		obj.n1();
-		System.out.println("======false=========");
-		obj.m9();
-		obj.m10();
-	}
 
-	private void n1() {
-		
-
-		String fullmsg="test: SVANidhi se Samriddhi programme, you are eligible for test test test test test test test test -MoHUA";
-		String template="{#var#}: SVANidhi se Samriddhi programme, you are eligible for {#var#}{#var#}{#var#}{#var#}{#var#}{#var#}{#var#}{#var#} -MoHUA";
-		
-	System.out.println(isMatch(template,fullmsg));
-
-		
-	}
-
-	private  void m10() {
-		String fullmsg="Dear test test, we are pleased to inform you that we have reduced our rates for the same coverage selected by you. We have initiated a partial refund of Rs 2000. Your new policy number is 222222 against Order ID 111111. Download your policy certificate by clicking on http:www.gmail.com Please call 18002666 for queries";
-		String template="dear {#var#}, your new policy number is {#var#} against order id {#var#}. download your policy certificate by clicking on {#var#} please call 18002666 for queries";
-		
-	System.out.println(isMatch(template,fullmsg));
-}
-	private  void m9() {
-		String fullmsg="Dear test test, we are pleased to inform you that we have reduced our rates for the same coverage selected by you. We have initiated a partial refund of Rs 2000. Your new policy number is 222222 against Order ID 111111. Download your policy certificate by clicking on http:www.gmail.com Please call 18002666 for queries* https://bit.ly/39KROPo";
-		String template="Dear {#var#}, your new policy number is {#var#} against Order ID {#var#}. Download your policy certificate by clicking on {#var#} Please call 18002666 for queries";
-		
-	System.out.println(isMatch(template,fullmsg));
-}
-	private  void m8() {
-		String fullmsg="Hi Livin Udyavara raw, Thank  you for purchasing and registering with Yokohama India Online warranty system through JAES WHEEL  Your Warranty Registration number is S486483. Please save this for all future references. 1 T&C* https://bit.ly/39KROPo";
-		String template="Hi {#var#}, Thank you for purchasing and registering with Yokohama India Online warranty system through {#var#} Your Warranty Registration number is {#var#} Please save this for all future references. {#var#}";
-		
-	System.out.println(isMatch(template,fullmsg));
-}	
-	private  void m7() {
-		String fullmsg="Please share code 11111 with Sales Executive of MPS Jeep for conducting Test Drive. Please provide Test Drive Feedback - test test";
-		String template ="Please share code {#var#} with Sales Executive of MPS Jeep for conducting Test Drive.";
-	
-	System.out.println(isMatch(template,fullmsg));
-}
-
-	private  void m6() {
-			String fullmsg="Dear Praveen Your complaint is registered and your complaint ticket number is 22222 - Yokohama India";
-		String template="Dear {#var#} Your complaint is registered and your complaint ticket number is {#var#} - Yokohama India";
-
-		System.out.println(isMatch(template,fullmsg));
-	}
-	
-	private  void m5() {
-		String fullmsg ="Dear DC Reader, Greetings! Please click below to view and read your favourite DC http://dcnews.deccanchr";
-		String template="Dear DC Reader, Greetings! Please click below to view and read your favourite DC {#var#}";
-
-		System.out.println(isMatch(template,fullmsg));
-	}
-	private  void m4() {
-		String fullmsg="Dear Subscriber, \n\n   Please verify OTP for Password change for Bhima Riddhi App.. OTP for verification is 123456";
-		String template="Dear Subscriber, Please verify OTP for Password change for Bhima Riddhi App.. OTP for verification is Rs.{#var#}S ";
-
-		System.out.println(isMatch(template,fullmsg));
-	}
-	private  void m1() {
-		String template="Dear {#var#}, we are pleased to inform you that we have reduced our rates for the same coverage selected by you. We have initiated a partial refund of Rs {#var#}. Your new policy number is {#var#} against Order ID {#var#}. Download your policy certificate by clicking on {#var#} Please call 18002666 for queries";
-		String fullmsg="Dear test test, we are pleased to inform you that we have reduced our rates for the same coverage selected by you. We have initiated a partial refund of Rs 2000. Your new policy number is 222222 against Order ID 111111. Download your policy certificate by clicking on http:www.gmail.com Please call 18002666 for queries";
-
-		System.out.println(isMatch(template,fullmsg));
-	}
-	
-	
-	private  void m2() {
-		String template ="Dear {#var#}, for issuance of a motor policy for Order ID {#var#}, your policy will not be generated until you fill the remaining details on {#var#} Your cover will not start tomorrow if you don't do so by 11:59 pm, tonight.";
-		String fullmsg="Dear 11111, for issuance of a motor policy for Order ID 11111, your policy will not be generated until you fill the remaining details on 22222 Your cover will not start tomorrow if you don't do so by 11:59 pm, tonight.";
-
-		System.out.println(isMatch(template,fullmsg));
-	}
-	
-	private  void m3() {
-			String template="Dear {#var#}, for issuance of a motor policy for Order ID {#var#}, your policy will not be generated until you fill the remaining details on {#var#}";
-			String fullmsg="Dear 111111, for issuance of a motor policy for Order ID 22222, your policy will not be generated until you fill the remaining details on 22222";
-
-		System.out.println(isMatch(template,fullmsg));
 	}
 public  boolean isMatch(String template,String fullmsg){
 		
@@ -138,9 +50,49 @@ public  boolean isMatch(String template,String fullmsg){
 			
 		
 		} 
-		
-		return true;
+/*
+		System.out.println(" msgpointer "+ msgpointer);
+		System.out.println(" msg.length "+ msg.length);
+		System.out.println(" temp.length "+ temp.length);
+*/
+		if(msg.length==temp.length){
+			
+			return true;
+			
+		}else if(msg.length>temp.length){
+			
+			
+			String t=temp[temp.length-1];
+			if("{#var#}".equals(t) || t.indexOf("{#var#}")>-1 ){
+			
+				if(msgpointer==msg.length-1){
+					
+					return true;
 
+				
+				
+				}else{
+					
+					return false;
+				}
+			}else{
+		
+			if(msgpointer==msg.length){
+		
+				return true;
+
+			
+			
+			}else{
+				
+				return false;
+			}
+			
+			}
+		}else{
+			
+			return true;
+		}
 		
 }
 
@@ -160,7 +112,13 @@ private int getUpcomingMsgPointer(int lastTempPointer, String[] temp, String[] m
 	
 	int temppointer=lastTempPointer;
 	
+	
 	temppointer++;
+	
+	if(temppointer==temp.length){
+		
+		return msg.length-1;
+	}
 	
 	if(temppointer<temp.length){
 		
