a51358c63fe192ed77a55b7b4e1967334fada73a
Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>
2021-04-08 21:36:48 +0530

    Signed-off-by: Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>

---
+++ queuecheck/src/main/java/queuecheck/THttpDN.java
@@ -0,0 +1,37 @@
+package queuecheck;
+
+public class THttpDN  extends Thread{
+
+	long start=System.currentTimeMillis();
+	
+	public void run(){
+		
+		while(true){
+			
+			try{
+				HttpDNCount.getInstance().doProcess();
+				gotosleep();
+			}catch(Exception e){
+				
+				e.printStackTrace();
+			}
+		}
+	}
+
+	
+
+	private void gotosleep() {
+
+
+		try{
+			Thread.sleep(10000L);
+		}catch(Exception e){
+			
+		}
+	}
+}
+
+
+
+
+
