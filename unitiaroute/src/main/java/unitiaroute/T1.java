5f8809ebf9b2a64119a5e7585c3ba685d7c36fc7
Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>
2021-04-17 17:59:05 +0530

    Signed-off-by: Jaffer Sadhik K.M. <jaffer.sadhik@gmail.com>

--- unitiaroute/src/main/java/unitiaroute/T1.java
+++ unitiaroute/src/main/java/unitiaroute/T1.java
@@ -11,164 +11,178 @@ public  boolean isMatch(String template,String fullmsg){
 		
 		String temp[]=StringUtils.split(template);
 		String msg[]=StringUtils.split(fullmsg);
-
-		int msgpointer=0;
-
-		for(int i=0;i<temp.length;i++){
-			
-			if(msgpointer>=msg.length){
-				
-				return  false;
-			}
-			String m=msg[msgpointer];
-			String t=temp[i];
-			if("{#var#}".equals(t) || t.indexOf("{#var#}")>-1 ){
-				int lastTempPointer=getLastTempPointer(temp,i);
-
-				int maxVarCharCount=getMaxVarcharCount(lastTempPointer,temp);
-				int upcomingmsgpointer=getUpcomingMsgPointer(lastTempPointer,temp,msg,msgpointer);
-				String varchar=getVarcharCharacter(msg,msgpointer,upcomingmsgpointer,lastTempPointer,temp);
-				int varcharcount=varchar.length();
-
-				String tempvarchar=getTempVarchar(i,lastTempPointer,temp);
 		
-				//System.out.println(" i "+ i);
-				//System.out.println(" tempvarchar "+ tempvarchar);
-
-				if((i+1)<temp.length){
-					//System.out.println(" next temp "+ temp[i+1]);
-
-				}else{
-					
-					//System.out.println(" next temp "+ "");
-
-				}
-				
-				if((upcomingmsgpointer)<msg.length){
-					//System.out.println(" upcoming msg "+ msg[upcomingmsgpointer]);
-
-				}else{
-					
-					//System.out.println(" upcoming msg "+ "");
-
-				}
-				
-				if((msgpointer+1)<msg.length){
-					//System.out.println(" next msg "+ msg[msgpointer+1]);
-
-				}else{
-					
-					//System.out.println(" next msg "+ "");
-
-				}
-				//System.out.println(" lastTempPointer "+ lastTempPointer);
-				//System.out.println(" maxVarCharCount "+ maxVarCharCount);
-				//System.out.println(" upcomingmsgpointer "+ upcomingmsgpointer);
-				//System.out.println(" varcharcount "+ varcharcount);
-				//System.out.println(" varchar "+ varchar);
-				//System.out.println(" msgpointer "+ msgpointer);
-				//System.out.println(" msg.length "+ msg.length);
-				//System.out.println(" temp.length "+ temp.length);
-
-	
-				String	prefixingcharacter=getPrefixingCharacter(tempvarchar);
-
-				
-									//System.out.println(" prefixingcharacter "+ prefixingcharacter);
-
-					
-					if(prefixingcharacter!=null&&prefixingcharacter.trim().length()>0&&!varchar.startsWith(prefixingcharacter)){
-						
-						return false;
-					}
-					
-					String	suffixingcharacter=getSufffixingCharacter(tempvarchar);
-					
-					//System.out.println(" suffixingcharacter "+ suffixingcharacter);
-
-					
-
-					
-					if(suffixingcharacter!=null&&suffixingcharacter.trim().length()>0&&!varchar.endsWith(suffixingcharacter)){
-						return false;
-					}
-					
-
-					List<String> intersectlist =getIntserSectList(tempvarchar);
-					
-					if(intersectlist!=null){
-						//System.out.println("intersectlist : "+intersectlist);
-						
-					
-						for(int j=0;j<intersectlist.size();j++){
-							
-							String s=intersectlist.get(j);
-
-							if(s!=null&&s.trim().length()>0){
-							if(!(varchar.indexOf(s.trim())>-1)){
-
-								//System.out.println(s);
-								return false;
-							}else{
-								
-								maxVarCharCount=maxVarCharCount+s.length();
-
-							}
-							}
-							
-						}
-					}
-					
-					i=lastTempPointer;
-
-				
-		
-		
-				if(maxVarCharCount>varcharcount){
-					msgpointer=upcomingmsgpointer;
-				}else{
-					return false;
-				}
-
-			}else if(! m.equalsIgnoreCase(t)){
-				
-				//System.out.println("temp : "+t);
-				//System.out.println("msg : "+m);
-
-				return false;
-			}else{
-				msgpointer++;
-			}
-
-			
-		
-		} 
-/*
-*/
-		if(msg.length==temp.length){
+		boolean result=isMatch(temp, msg);
+		if(result){
 			
 			return true;
+		}
+		return isMatch(reverse(temp),reverse( msg));
+}
+
+private String [] reverse(String [] param){
+	
+	String  [] result=new String[param.length];
+	int j=0;
+	for(int i=param.length-1;i>=0;i--){
+		
+		result[j++]=param[i];
+		
+		
+	}
+	
+	
+	return result;
+}
+private boolean isMatch(String temp[],String msg[]){
+	
+	
+
+	int msgpointer=0;
+
+	for(int i=0;i<temp.length;i++){
+		
+		if(msgpointer>=msg.length){
 			
-		}else if(msg.length>temp.length){
+			return  false;
+		}
+		String m=msg[msgpointer];
+		String t=temp[i];
+		if("{#var#}".equals(t) || t.indexOf("{#var#}")>-1 ){
+			int lastTempPointer=getLastTempPointer(temp,i);
+
+			int maxVarCharCount=getMaxVarcharCount(lastTempPointer,temp);
+			int upcomingmsgpointer=getUpcomingMsgPointer(lastTempPointer,temp,msg,msgpointer);
+			String varchar=getVarcharCharacter(msg,msgpointer,upcomingmsgpointer,lastTempPointer,temp);
+			int varcharcount=varchar.length();
+
+			String tempvarchar=getTempVarchar(i,lastTempPointer,temp);
+	
+			//System.out.println(" i "+ i);
+			//System.out.println(" tempvarchar "+ tempvarchar);
+
+			if((i+1)<temp.length){
+				//System.out.println(" next temp "+ temp[i+1]);
+
+			}else{
+				
+				//System.out.println(" next temp "+ "");
+
+			}
 			
+			if((upcomingmsgpointer)<msg.length){
+				//System.out.println(" upcoming msg "+ msg[upcomingmsgpointer]);
+
+			}else{
+				
+				//System.out.println(" upcoming msg "+ "");
+
+			}
 			
-			String t=temp[temp.length-1];
-			if("{#var#}".equals(t) || t.indexOf("{#var#}")>-1 ){
+			if((msgpointer+1)<msg.length){
+				//System.out.println(" next msg "+ msg[msgpointer+1]);
+
+			}else{
+				
+				//System.out.println(" next msg "+ "");
+
+			}
+			//System.out.println(" lastTempPointer "+ lastTempPointer);
+			//System.out.println(" maxVarCharCount "+ maxVarCharCount);
+			//System.out.println(" upcomingmsgpointer "+ upcomingmsgpointer);
+			//System.out.println(" varcharcount "+ varcharcount);
+			//System.out.println(" varchar "+ varchar);
+			//System.out.println(" msgpointer "+ msgpointer);
+			//System.out.println(" msg.length "+ msg.length);
+			//System.out.println(" temp.length "+ temp.length);
+
+
+			String	prefixingcharacter=getPrefixingCharacter(tempvarchar);
+
 			
-				if(msgpointer==msg.length-1){
-					
-					return true;
+								//System.out.println(" prefixingcharacter "+ prefixingcharacter);
 
 				
-				
-				}else{
+				if(prefixingcharacter!=null&&prefixingcharacter.trim().length()>0&&!varchar.startsWith(prefixingcharacter)){
 					
 					return false;
 				}
+				
+				String	suffixingcharacter=getSufffixingCharacter(tempvarchar);
+				
+				//System.out.println(" suffixingcharacter "+ suffixingcharacter);
+
+				
+
+				
+				if(suffixingcharacter!=null&&suffixingcharacter.trim().length()>0&&!varchar.endsWith(suffixingcharacter)){
+					return false;
+				}
+				
+
+				List<String> intersectlist =getIntserSectList(tempvarchar);
+				
+				if(intersectlist!=null){
+					//System.out.println("intersectlist : "+intersectlist);
+					
+				
+					for(int j=0;j<intersectlist.size();j++){
+						
+						String s=intersectlist.get(j);
+
+						if(s!=null&&s.trim().length()>0){
+						if(!(varchar.indexOf(s.trim())>-1)){
+
+							//System.out.println(s);
+							return false;
+						}else{
+							
+							maxVarCharCount=maxVarCharCount+s.length();
+
+						}
+						}
+						
+					}
+				}
+				
+				i=lastTempPointer;
+
+			
+	
+	
+			if(maxVarCharCount>varcharcount){
+				msgpointer=upcomingmsgpointer;
 			}else{
+				return false;
+			}
+
+		}else if(! m.equalsIgnoreCase(t)){
+			
+			//System.out.println("temp : "+t);
+			//System.out.println("msg : "+m);
+
+			return false;
+		}else{
+			msgpointer++;
+		}
+
 		
-			if(msgpointer==msg.length){
+	
+	} 
+/*
+*/
+	if(msg.length==temp.length){
 		
+		return true;
+		
+	}else if(msg.length>temp.length){
+		
+		
+		String t=temp[temp.length-1];
+		if("{#var#}".equals(t) || t.indexOf("{#var#}")>-1 ){
+		
+			if(msgpointer==msg.length-1){
+				
 				return true;
 
 			
@@ -177,13 +191,26 @@ public  boolean isMatch(String template,String fullmsg){
 				
 				return false;
 			}
-			
-			}
+		}else{
+	
+		if(msgpointer==msg.length){
+	
+			return true;
+
+		
+		
 		}else{
 			
-			return true;
+			return false;
 		}
 		
+		}
+	}else{
+		
+		return true;
+	}
+	
+
 }
 private List<String> getIntserSectList(String tempvarchar) {
 	
